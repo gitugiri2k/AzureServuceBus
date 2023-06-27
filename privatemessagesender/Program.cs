2c1facc6f34fee4019c75df2e3051281b17a6294
Girish <gitugiri2k@gmail.com>
2023-06-27 02:25:41 -0500

    Azure Service Bus for Pubsub Cycle Company

---
+++ privatemessagesender/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+namespace privatemessagesender
+{
+    class Program
+    {
+
+        const string ServiceBusConnectionString = "Endpoint=sb://salesteamappcontoso2022gm.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=4s/f+Ug/4pRIFXee9o9dnhMO6AoRT7mB6+ASbEZ1Hs0=";
+        const string QueueName = "salesmessages";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sending a message to the Sales Messages queue...");
+
+            SendSalesMessageAsync().GetAwaiter().GetResult();
+
+            Console.WriteLine("Message was sent successfully.");
+        }
+
+        static async Task SendSalesMessageAsync()
+        {
+            // By leveraging "await using", the DisposeAsync method will be called automatically once the client variable goes out of scope. 
+            // In more realistic scenarios, you would want to store off a class reference to the client (rather than a local variable) so that it can be used throughout your program.
+
+            await using var client = new ServiceBusClient(ServiceBusConnectionString);
+            await using ServiceBusSender sender = client.CreateSender(QueueName);
+            try
+            {
+                string messageBody = $"$10,000 order for bicycle parts from retailer Adventure Works.";
+                var message = new ServiceBusMessage(messageBody);
+                Console.WriteLine($"Sending message: {messageBody}");
+                await sender.SendMessageAsync(message);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+
+            finally
+            {
+                // Calling DisposeAsync on client types is required to ensure that network
+                // resources and other unmanaged objects are properly cleaned up.
+                await sender.DisposeAsync();
+                await client.DisposeAsync();
+            }
+        }
+    }
+}
